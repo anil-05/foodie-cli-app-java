7dac7afaf2f6af635223db3c7da6e2d894eee5ae
Anil <anilshiliveru@gmail.com>
2024-07-02 23:22:45 +0530

    Read and Processed Customer csv file

---
+++ src/com/trainingmug/foodiecliap/Main.java
@@ -0,0 +1,10 @@
+package com.trainingmug.foodiecliap;
+
+import com.trainingmug.foodiecliap.util.CsvReader;
+
+public class Main {
+    public static void main(String[] args) {
+        CsvReader csvReader = new CsvReader();
+        System.out.println(csvReader.readCustomersFromCsv());
+    }
+}
---
+++ src/com/trainingmug/foodiecliap/util/CsvReader.java
@@ -0,0 +1,47 @@
+package com.trainingmug.foodiecliap.util;
+
+
+import com.trainingmug.foodiecliap.model.Customer;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class CsvReader {
+    /*
+    Read the data from CSV files and create a List of objects
+     */
+    public List<Customer> readCustomersFromCsv(){
+        String customerCsvFilePath  = "C:\\Users\\ADMIN\\IdeaProjects\\foodie-cli-app-java\\data\\customers.csv";
+        List<Customer> customerList = new ArrayList<>();
+        //java io classes (File Reader, BufferedReader)
+        //try-with-resource
+        String line;
+        try (BufferedReader br = new BufferedReader(new FileReader(customerCsvFilePath))) {
+            String csvSplitBy = ",";
+            br.readLine();
+            while((line = br.readLine()) != null){
+                //c001,john doe, john.doe@gmail.com, johndoe@1234
+                String[] data = line.split(csvSplitBy);
+                Customer customer = new Customer();
+                customer.setId(data[0]);
+                customer.setName(data[1]);
+                customer.setEmail(data[2]);
+                customer.setPassword(data[3]);
+
+                customerList.add(customer);
+            }
+
+
+        } catch(IOException e){
+            System.out.println("File not found in the path : "+ customerCsvFilePath );
+            System.exit(0);
+            e.printStackTrace();
+        }
+
+        return customerList;
+
+    }
+}
