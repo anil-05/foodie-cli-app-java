c926739f306ecea3d2eff444747334d4d4f6f95a
Anil <anilshiliveru@gmail.com>
2024-07-05 22:51:45 +0530

    feat: "Add Menu to ui"

--- src/com/trainingmug/foodiecliap/Main.java
+++ src/com/trainingmug/foodiecliap/Main.java
@@ -4,22 +4,14 @@ import com.trainingmug.foodiecliap.model.Dish;
 import com.trainingmug.foodiecliap.repository.CustomerRepository;
 import com.trainingmug.foodiecliap.repository.DishRepository;
 import com.trainingmug.foodiecliap.repository.RestaurantRepository;
+import com.trainingmug.foodiecliap.ui.Menu;
 import com.trainingmug.foodiecliap.util.CsvReader;
 
 public class Main {
     public static void main(String[] args) {
-        //CsvReader csvReader = new CsvReader();
-        //System.out.println(csvReader.readCustomersFromCsv());
 
-        //CustomerRepository customerRepository = new CustomerRepository();
-        //System.out.println(customerRepository.getAllCustomers());
-
-        /*DishRepository dishRepository = new DishRepository();
-        System.out.println(dishRepository.getAllDishes());
-         */
-
-        RestaurantRepository restaurantRepository = new RestaurantRepository();
-        System.out.println(restaurantRepository.getAllRestaurants());
+        Menu menu = new Menu();
+        menu.displayMenu();
     }
 
 }
